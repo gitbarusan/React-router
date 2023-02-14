cbe1d2a899780388a8b83a21d7d0a7a6eb828d8d
gitbarusan <101911557+gitbarusan@users.noreply.github.com>
2023-02-15 05:31:33 +0900

    jsを使った遷移　404作成

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,7 +1,17 @@
+import { useLocation, useHistory } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const { state } = useLocation();
+  console.log(state);
+
+  const history = useHistory();
+  //遷移元のページに戻る
+  const onClickBack = () => history.goBack();
   return (
     <div>
       <h1>Page1DetailAページです</h1>
+      <br />
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
